09fa821665e3dcf4368606f2a76086348fc7850c
BobZhang <BobZhang@bobzhangdemac-mini.local>
2016-06-01 11:16:42 +0800

    Initial Commit

---
+++ UIDevice-C14/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIDevice-C14
+//
+//  Created by BobZhang on 16/6/1.
+//  Copyright © 2016年 BobZhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIDevice-C14/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIDevice-C14
+//
+//  Created by BobZhang on 16/6/1.
+//  Copyright © 2016年 BobZhang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
